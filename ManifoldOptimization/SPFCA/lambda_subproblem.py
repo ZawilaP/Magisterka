33eff2929f291417050bae2fe5f6f0eebb42c69b
ZawilaP <piotrzawilaniedzwiecki@gmail.com>
2020-03-22 13:43:57 +0100

    created lambda_subproblem.py

---
+++ ManifoldOptimization/SPFCA/lambda_subproblem.py
@@ -0,0 +1,7 @@
+class LambdaSubproblem(lambda_2, V_matrix, X_matrix):
+    '''
+    This part is just a regular Lasso optimization problem but in a context of matrices from Stiefel manifold.
+
+    Given input matrices we want to compute:
+    \Lambda^{k+1} = \argmin _{\boldsymbol{V} \in  \mathcal{V}_{\boldsymbol{p} \times \boldsymbol{p}}^{\boldsymbol{S} \boldsymbol{u}}} \left\|V^{kT}XV^{k}- \Lambda \right\|_{F}^{2} +  \lambda_{2}\|\Lambda\|_{2}
+    '''
