04fe04eadbe720d17a28df73a696cad2ec5ad6a7
ZawilaP <piotrzawilaniedzwiecki@gmail.com>
2020-03-22 15:49:22 +0100

    Initiatied MADMM class

--- ManifoldOptimization/SPFCA/V_subproblem.py
+++ ManifoldOptimization/SPFCA/V_subproblem.py
@@ -2,3 +2,13 @@ from ManifoldOptimization.Utils.utils import get_matrix_diagonal, get_matrix_mul
 import numpy as np
 
 class MADMM():
+    '''
+    This algorithm has two steps, that require their own algorithms.
+
+    W step - similar to how we solved lambda_subproblem
+
+    V step - which requires a lot of of matrix operations
+    '''
+
+    def init(self):
+        print("to be implemented still")
