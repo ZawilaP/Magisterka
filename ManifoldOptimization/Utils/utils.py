990f521e2b2a8682bbf57ed125988381a99ea337
ZawilaP <piotrzawilaniedzwiecki@gmail.com>
2020-03-22 13:43:32 +0100

    Added matrix_multiplication

--- ManifoldOptimization/Utils/utils.py
+++ ManifoldOptimization/Utils/utils.py
@@ -1,5 +1,11 @@
+import numpy as np
+
 class utils:
     def safe_division(self, nominator, denominator):
         safe_division_constant = 0.0000001
-        safe_divided = nominator/(denominator+safe_division_constant)
+        safe_divided = nominator / (denominator + safe_division_constant)
         return safe_divided
+
+    def matrix_multiplication(self, left_matrix, right_matrix):
+        multiplied = np.dot(left_matrix, right_matrix)
+        return multiplied
