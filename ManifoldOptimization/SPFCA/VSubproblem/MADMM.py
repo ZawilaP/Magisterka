df18d23f65932978fb76f8f78992ac805533cf98
ZawilaP <piotrzawilaniedzwiecki@gmail.com>
2020-03-22 16:02:23 +0100

    Added docstring

--- ManifoldOptimization/SPFCA/VSubproblem/MADMM.py
+++ ManifoldOptimization/SPFCA/VSubproblem/MADMM.py
@@ -3,11 +3,13 @@ import numpy as np
 
 class MADMM():
     '''
-    This algorithm has two steps, that require their own algorithms.
+    This algorithm has two steps, that require their own algorithms, and one simple step
 
     W step - similar to how we solved lambda_subproblem
 
     V step - which requires a lot of of matrix operations
+
+    Z step - simple matrix subtraction and addition
     '''
 
     def __init__(self):
