12bf97d3206009e8cb7ab401e89f709ab70f1156
ZawilaP <piotrzawilaniedzwiecki@gmail.com>
2020-03-22 13:50:18 +0100

    added matrix_transpose

--- ManifoldOptimization/Utils/utils.py
+++ ManifoldOptimization/Utils/utils.py
@@ -1,11 +1,15 @@
 import numpy as np
 
-class utils:
-    def safe_division(self, nominator, denominator):
-        safe_division_constant = 0.0000001
-        safe_divided = nominator / (denominator + safe_division_constant)
-        return safe_divided
 
-    def matrix_multiplication(self, left_matrix, right_matrix):
-        multiplied = np.dot(left_matrix, right_matrix)
-        return multiplied
+def safe_division(nominator, denominator):
+    safe_division_constant = 0.0000001
+    safe_divided = nominator / (denominator + safe_division_constant)
+    return safe_divided
+
+
+def matrix_multiplication(left_matrix, right_matrix):
+    return np.dot(left_matrix, right_matrix)
+
+
+def matrix_transpose(matrix_to_transpose: np.array):
+    return matrix_to_transpose.transpose()
