f31240e7e873afbed239f82f704beeece3f3ca6f
ZawilaP <piotrzawilaniedzwiecki@gmail.com>
2020-03-22 15:26:01 +0100

    Added V_subproblem.py

---
+++ ManifoldOptimization/SPFCA/V_subproblem.py
@@ -0,0 +1,4 @@
+from ManifoldOptimization.Utils.utils import get_matrix_diagonal, get_matrix_multiplication, get_matrix_transpose, reconstruct_vector_into_diagonal_matrix
+import numpy as np
+
+class MADMM():
