07e72fc6fb9e711b9690c187306ba504a1926242
ZawilaP <piotrzawilaniedzwiecki@gmail.com>
2020-03-22 14:26:36 +0100

    Added call that returns our new lambda_k, class computes everything on __init__

--- ManifoldOptimization/SPFCA/lambda_subproblem.py
+++ ManifoldOptimization/SPFCA/lambda_subproblem.py
@@ -1,4 +1,5 @@
-from ManifoldOptimization.Utils.utils import *
+from ManifoldOptimization.Utils.utils import get_matrix_diagonal, get_matrix_multiplication, get_matrix_transpose
+
 
 class LambdaSubproblem():
     '''
@@ -7,7 +8,30 @@ class LambdaSubproblem():
     Given input matrices we want to compute:
     \Lambda^{k+1} = \argmin _{\boldsymbol{V} \in  \mathcal{V}_{\boldsymbol{p} \times \boldsymbol{p}}^{\boldsymbol{S} \boldsymbol{u}}} \left\|V^{kT}XV^{k}- \Lambda \right\|_{F}^{2} +  \lambda_{2}\|\Lambda\|_{2}
     '''
+
     def __init__(self, lambda_2, V_matrix, X_matrix):
         self.lambda_constant = lambda_2
         self.V = V_matrix
         self.X = X_matrix
+        self.Z = self.compute_z()
+        self.lambda_k = self.compute_new_lambda_k()
+        self.reconstructed_lambda_k = self.reconstruct_vector_into_diagonal_matrix()
+
+    def __call__(self, *args, **kwargs):
+        return self.reconstructed_lambda_k
+
+    def compute_z(self):
+        V_transposed = get_matrix_transpose(self.V)
+        Z = get_matrix_multiplication(get_matrix_multiplication(V_transposed, X), self.V)
+        return Z
+
+    def compute_new_lambda_k(self):
+        def get_max(Z_element):
+            return max([0, Z_element - (self.lambda_constant / 2)])
+
+        Z_diagonal = get_matrix_diagonal(Z)
+        lambda_k = map(get_max, Z_diagonal)
+        return np.array(lambda_k)
+
+    def reconstruct_vector_into_diagonal_matrix(self):
+        return np.identity(len(self.lambda_k)) * np.outer(np.ones(len(self.lambda_k)), self.lambda_k)
