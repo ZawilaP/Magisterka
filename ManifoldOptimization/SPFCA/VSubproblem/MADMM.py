cb5326e1972a625244788dc3295b8fdd51f4e507
ZawilaP <piotrzawilaniedzwiecki@gmail.com>
2020-03-29 16:26:17 +0200

    Added execute MADMM

--- ManifoldOptimization/SPFCA/VSubproblem/MADMM.py
+++ ManifoldOptimization/SPFCA/VSubproblem/MADMM.py
@@ -1,4 +1,5 @@
 from ManifoldOptimization.Utils.matrix_operations import get_matrix_diagonal, get_matrix_multiplication, get_matrix_transpose, reconstruct_vector_into_diagonal_matrix
+from ManifoldOptimization.SPFCA.VSubproblem.MADMM_Subproblems import WSubproblem, VSubProblem, ZSubProblem
 import numpy as np
 
 class MADMM():
@@ -12,15 +13,28 @@ class MADMM():
     Z step - simple matrix subtraction and addition
     '''
 
-    def __init__(self, Lambda_Matrix, X_matrix, rho, lambda_1):
+    def __init__(self, Lambda_Matrix, X_matrix, rho, lambda_1, n_steps):
         self.V_hat = V_matrix
         self.Lambda = Lambda_Matrix
         self.X = X_matrix
         self.rho = rho
         self.lambda_1 = lambda_1
+        self.n_steps = n_steps
         self.initialize_variables()
+        self.new_V = self.execute_MADMM()
 
     def initialize_variables(self):
         self.V_k = self.V_hat
         self.W_k = self.V_hat
         self.Z_k = np.zeros(self.Lambda.shape(0))
+
+    def execute_MADMM(self):
+        step_W = self.W_k
+        step_V = self.V_k
+        step_Z = self.Z_k
+        for step in self.n_steps:
+            step_V = VSubProblem(step_Z, step_W, self.Lambda, self.X_matrix, self.rho)
+            step_W = WSubproblem(self.lambda_1, step_V, step_Z, step_W, self.rho)
+            step_Z = ZSubProblem(step_Z, step_V, step_W)
+
+        return step_V
