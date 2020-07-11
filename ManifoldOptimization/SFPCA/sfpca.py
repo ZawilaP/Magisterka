cfd322e2e49434135c708d8f57d62ee734931a2a
ZawilaP <piotrzawilaniedzwiecki@gmail.com>
2020-07-11 15:27:49 +0200

    Cleaned repo, added Unit Tests and try catching to matrix_operations.py

--- ManifoldOptimization/SFPCA/sfpca.py
+++ ManifoldOptimization/SFPCA/sfpca.py
@@ -1,47 +1,4 @@
 import numpy as np
 
-from ManifoldOptimization.SFPCA.LambdaSubproblem.lambda_subproblem import LambdaSubproblem
-from ManifoldOptimization.SFPCA.VSubproblem.madmm import MADMM
-from ManifoldOptimization.Utils.matrix_operations import reconstruct_vector_into_diagonal_matrix, \
-    get_matrix_multiplication, get_matrix_transpose, get_matrix_diagonal
-
 
 class SFPCA():
-
-    def __init__(self, madmm_steps, sfpca_steps, input_matrix, rho, lambda_1, lambda_2):
-        self.madmm_steps = madmm_steps
-        self.sfpca_steps = sfpca_steps
-        self.input_matrix = input_matrix
-        self.rho = rho
-        self.lambda_1 = lambda_1
-        self.lambda_2 = lambda_2
-        self.X = get_matrix_multiplication(self.input_matrix, get_matrix_transpose(self.input_matrix))
-
-    def __call__(self, *args, **kwargs):
-        return self.execute_sfpca()
-
-    # todo: czy zakładać, że macierz wejściowa jest od razu kwadratowa, czy zrobić to na różne przypadki?
-    # todo: ktora macierz normalizowac? Wejsciowa? XXT?
-
-    def get_spectral_decomposition(self):
-        """
-        Function that gets decomposition of the input matrix
-        :return: eigenvalues, and eigenvectors
-        """
-        eigenvalues, eigenvectors = np.linalg.eig(self.X)
-        Lambda_matrix = reconstruct_vector_into_diagonal_matrix(eigenvalues)
-        return Lambda_matrix, eigenvectors
-
-    def execute_sfpca(self):
-        """
-        Function that executes whole algorithm using given input
-        :return: eigenvalues vector, and eigenvectors matrix
-        """
-        step_lambda, step_v = self.get_spectral_decomposition()
-        for step in range(self.sfpca_steps):
-            step_lambda = LambdaSubproblem(self.lambda_2, step_v, self.X)
-            step_v = MADMM(step_v, step_lambda, self.X, self.rho, self.lambda_1, self.madmm_steps)
-
-        return get_matrix_diagonal(step_lambda), step_v
-
-    #todo: Czy zwracać zrekonstruowaną macierz?
--- ManifoldOptimization/Utils/matrix_operations.py
+++ ManifoldOptimization/Utils/matrix_operations.py
@@ -4,35 +4,43 @@ import pandas as pd
 
 def safe_division(nominator: np.array, denominator: np.array) -> np.array:
     """
-    Function that safely divides two numbers, so we don't divide by zero
+    Function that safely divides two arrays, so we don't divide by zero
     :param nominator:   'float' nominator of expression
     :param denominator: 'float' denominator of expression
     :return: safely divided nominator by denominator
     """
-    safe_division_constant = 0.0000001
-    safe_divided = nominator / (denominator + safe_division_constant)
+    safe_division_constant = 0.000001
+    safe_divided = (nominator / (denominator + safe_division_constant)) if (
+        np.any(denominator == 0)) else (nominator / denominator)
     return safe_divided
 
 
-def get_matrix_multiplication(left_matrix: np.array, right_matrix: np.array) -> np.array:
+def multiply_matrices(left_matrix: np.array, right_matrix: np.array) -> np.array:
     """
     Multiply two matrices
     :param left_matrix:   'np.array' left side of multiplication
     :param right_matrix:  'np.array' right side of multiplication
     :return: multiplied left by right
     """
+    if left_matrix.shape != right_matrix.shape:
+        raise AttributeError("Both matrices need to be of the same shape")
     return np.dot(left_matrix, right_matrix)
 
-def get_matrix_inverse(matrix_to_inverse: np.array) -> np.array:
+
+def inverse_matrix(matrix_to_inverse: np.array) -> np.array:
     '''
-    Get inverse of matrix
+    Get inverse of matrix, in case of singular matrix compute pseudo-inverse
     :param matrix_to_inverse: 'np.array' matrix to get inverse
     :return: inversed matrix
     '''
-    return np.linalg.inv(matrix_to_inverse)
+    try:
+        return np.linalg.inv(matrix_to_inverse)
+    except np.linalg.LinAlgError:
+        print("Matrix non-invertible, using moore-penrose pseudo-inverse")
+        return np.linalg.pinv(matrix_to_inverse)
 
 
-def get_matrix_transpose(matrix_to_transpose: np.array) -> np.array:
+def transpose_matrix(matrix_to_transpose: np.array) -> np.array:
     """
     Get transposed matrix
     :param matrix_to_transpose:  'np.array' matrix to be transposed
@@ -41,7 +49,7 @@ def get_matrix_transpose(matrix_to_transpose: np.array) -> np.array:
     return matrix_to_transpose.transpose()
 
 
-def get_matrix_diagonal(matrix_to_get_diagonal: np.array) -> np.array:
+def matrix_diagonal(matrix_to_get_diagonal: np.array) -> np.array:
     '''
     Function that gets diagonal of a matrix
     :param matrix_to_get_diagonal: 'np.array' from which we extract diagonal
@@ -50,7 +58,7 @@ def get_matrix_diagonal(matrix_to_get_diagonal: np.array) -> np.array:
     return np.diagonal(matrix_to_get_diagonal)
 
 
-def get_matrix_sign(matrix_to_get_sign: np.array) -> np.array:
+def matrix_sign(matrix_to_get_sign: np.array) -> np.array:
     '''
     Function that gets elementwise signs of matrix elements
     :param matrix_to_get_sign: 'np.array' matrix to get elementwise sign values
@@ -76,6 +84,7 @@ def pandas_to_numpy(matrix_to_convert: pd.DataFrame) -> np.array:
     '''
     return matrix_to_convert.to_numpy()
 
+
 def elementwise_multiplication(left_matrix: np.array, right_matrix: np.array) -> np.array:
     '''
     Perform elementwise multiplication of two matrices
@@ -83,13 +92,28 @@ def elementwise_multiplication(left_matrix: np.array, right_matrix: np.array) ->
     :param right_matrix: 'np.array'
     :return: matrix from multiplication
     '''
+    if left_matrix.shape != right_matrix.shape:
+        raise AttributeError("Both matrices need to be of the same shape")
     return np.multiply(left_matrix, right_matrix)
 
 
-def reconstruct_vector_into_diagonal_matrix(lambda_k: np.array) -> np.array:
+def vector_into_diagonal_matrix(vector: np.array) -> np.array:
     '''
     Transform singular values vector into diagonal matrix
-    :param lambda_k: 'np.array' vector to be transformed into matrix
+    :param vector: 'np.array' vector to be transformed into matrix
     :return: matrix with diagonal with lambda_k values, 0's otherwise
     '''
-    return np.identity(len(lambda_k)) * np.outer(np.ones(len(lambda_k)), lambda_k)
+    return np.identity(len(vector)) * np.outer(np.ones(len(vector)), vector)
+
+
+def frobenius_norm(left_matrix: np.array, right_matrix: np.array) -> float:
+    """
+    Calculate frobenius norm of difference of two matrices
+    :param left_matrix: 'np.array'
+    :param right_matrix: 'np.array'
+    :return:
+    """
+    if left_matrix.shape != right_matrix.shape:
+        raise AttributeError("Both matrices need to be of the same shape")
+    matrix_difference = left_matrix - right_matrix
+    return np.linalg.norm(matrix_difference, ord="fro")
