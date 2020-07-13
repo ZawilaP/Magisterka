b2525a5530c463cbca955e88cee000112447d74c
ZawilaP <piotrzawilaniedzwiecki@gmail.com>
2020-07-13 17:27:47 +0200

    Added LambdaSubproblem with Unit Test

--- ManifoldOptimization/SFPCA/LambdaSubproblem/lambda_subproblem.py
+++ ManifoldOptimization/SFPCA/LambdaSubproblem/lambda_subproblem.py
@@ -1,5 +1,18 @@
-import numpy as np
+from ManifoldOptimization.Utils.matrix_operations import soft_thresholding
+
 
 class LambdaSubproblem():
+    """
+    Compute Lasso optimization problem for Lambda matrix, which is defined as:
+    $$\hat{\Lambda} = \argmin _{\Lambda \in \mathbb{R}_{+}^{p}} \left\|X-V \Lambda V^{\top}\right\|_{F}^{2} + \lambda_{2}\|\Lambda\|_{2}$$
+    Which simplifies to soft-thresholding Lambda matrix by lambda_2 regularization parameter, divided by two
+    """
 
+    def __init__(self, lambda_matrix, lambda_2):
+        self.lambda_matrix = lambda_matrix
+        self.lambda_2 = lambda_2
 
+    def fit(self):
+        division_constant = 2
+        new_lambda = soft_thresholding(self.lambda_matrix, self.lambda_2 / division_constant)
+        return new_lambda
---
+++ Test/LambdaSubproblemTest/lambda_subproblem_test.py
@@ -0,0 +1,26 @@
+import unittest
+
+import numpy as np
+
+from ManifoldOptimization.SFPCA.LambdaSubproblem.lambda_subproblem import LambdaSubproblem
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        print(self._testMethodDoc)
+
+    @staticmethod
+    def test_lambda_subproblem():
+        sample_lambda_matrix = np.array([[1, 0, 0], [0, 2, 0], [0, 0, 3]])
+        sample_lambda_2 = 2
+        initial_lambda = LambdaSubproblem(sample_lambda_matrix, sample_lambda_2)
+        final_lambda = initial_lambda.fit()
+        print("==> Showing final_lambda:")
+        print(final_lambda)
+        expected_result = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 2]])
+        np.testing.assert_array_equal(final_lambda, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
