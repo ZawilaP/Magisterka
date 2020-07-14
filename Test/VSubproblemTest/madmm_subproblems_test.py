9651a1c777e92ca52910245c2a567fcd99909a65
ZawilaP <piotrzawilaniedzwiecki@gmail.com>
2020-07-14 17:43:57 +0200

    Finished MADMM, but algorithm doesn't converge

--- Test/VSubproblemTest/madmm_subproblems_test.py
+++ Test/VSubproblemTest/madmm_subproblems_test.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from ManifoldOptimization.SFPCA.VSubproblem.madmm_subproblems import WSubproblem, VSubproblem
+from ManifoldOptimization.SFPCA.VSubproblem.madmm_subproblems import WSubproblem, VSubproblem, ZSubproblem, MADMM
 from ManifoldOptimization.Utils.matrix_operations import vector_into_diagonal_matrix
 
 
@@ -13,10 +13,10 @@ class MyTestCase(unittest.TestCase):
 
     @staticmethod
     def test_w_subproblem():
-        sample_w_matrix = np.array([[1, -1, 3], [11, 13, -11], [-3, 2, 9]])
-        sample_lambda = 4
-        sample_rho = 2
-        initialized_w = WSubproblem(sample_w_matrix, sample_lambda, sample_rho, verbosity=1)
+        w_matrix = np.array([[1, -1, 3], [11, 13, -11], [-3, 2, 9]])
+        lambda_1 = 4
+        rho = 2
+        initialized_w = WSubproblem(w_matrix, lambda_1, rho, verbosity=2)
         final_w = initialized_w.fit()
         print("==> Showing final_w")
         print(final_w)
@@ -31,7 +31,40 @@ class MyTestCase(unittest.TestCase):
         w_matrix = v_matrix
         z_matrix = np.zeros((3, 3))
         rho = 1 / 2
-        initialized_v = VSubproblem(w_matrix, z_matrix, x_matrix, v_matrix, lambda_matrix, rho, verbosity=1)
+        initialized_v = VSubproblem(w_matrix, z_matrix, x_matrix, v_matrix, lambda_matrix, rho, verbosity=2)
+        final_v = initialized_v.fit()
+        print("==> Showing final_v")
+        print(final_v)
+        expected_result = np.array([[38.591981, 26.723627, 85.750215],
+                                    [-7.750934, 11.882393, -0.214772],
+                                    [-10.830568, -6.937643, 7.036391]])
+        np.testing.assert_array_almost_equal(final_v, expected_result)
+
+    @staticmethod
+    def test_z_subproblem():
+        v_matrix = np.array([[38.591981, 26.723627, 85.750215],
+                             [-7.750934, 11.882393, -0.214772],
+                             [-10.830568, -6.937643, 7.036391]])
+        w_matrix = np.array([[1, -1, 3], [11, 13, -11], [-3, 2, 9]])
+        z_matrix = np.zeros((3, 3))
+        initialized_z = ZSubproblem(z_matrix, w_matrix, v_matrix)
+        final_z = initialized_z.fit()
+        print("==> Showing final_z")
+        print(final_z)
+        expected_result = np.array([[37.591981, 27.723627, 82.750215],
+                                    [-18.750934, -1.117607, 10.785228],
+                                    [-7.830568, -8.937643, -1.963609]])
+        np.testing.assert_array_almost_equal(final_z, expected_result)
+
+    @staticmethod
+    def test_madmm():
+        x_matrix = np.array([[1, -1, 3], [-1, 2, 2], [3, 2, 5]])
+        v_matrix, lambda_matrix_vector, _ = np.linalg.svd(x_matrix)
+        lambda_matrix = vector_into_diagonal_matrix(lambda_matrix_vector)
+        rho = 10
+        lambda_1 = 2
+        n_steps = 3
+        initialized_v = MADMM(x_matrix, v_matrix, lambda_matrix, lambda_1, rho, n_steps, verbosity=2)
         final_v = initialized_v.fit()
         print("==> Showing final_v")
         print(final_v)
